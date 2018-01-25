209dcd454918697b7b5ca9c04db6fcc21f28a95d
pwtail <abvit89@gmail.com>
2018-01-25 08:48:22 +0300

    -

--- lib/attrs.ex
+++ lib/attrs.ex
@@ -2,16 +2,23 @@ defmodule Greetify do
 
     defmacro __using__(_) do
       quote do
+        import Greetify
         Module.register_attribute __MODULE__, :greet, accumulate: true,
-          persist: false
+          persist: true
         @before_compile Greetify
       end
     end
   
     defmacro __before_compile__(env) do
       greetings = Module.get_attribute(env.module, :greet)
-      for {name, age} <- greetings do
-        IO.puts "#{name} is #{age} years old"
+      IO.inspect greetings
+    end
+
+    defmacro dep(head, body) do
+      
+      quote do
+        @greet unquote(1)
+        def(unquote(head), unquote(body))
       end
     end
   
@@ -20,8 +27,19 @@ end
 
 
 defmodule M do
-    use Greetify
+  use Greetify
 
-    @greet {"Jon", 21}
-    @greet {"Sam", 23}
+  dep f1 do
+    
+    1
+  end
+
+  dep f2 do
+    
+    1
+  end
+
+  dep f x do
+    
+  end
 end
